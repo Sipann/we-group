3fe9abaf5129a089637980b6ddf9691196d1e9ca
Sipann <vauguste92@gmail.com>
2020-05-21 16:26:24 +0200

    refactor: set api baseUrl for user.service.ts  in env file

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,15 +1,16 @@
-import { Group } from '../models/group.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { UserData } from '../store/actions/user.actions';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private baseUrl = 'http://localhost:3000';      //TODO from config file
+  private baseUrl = environment.apiBaseUrl;
 
   constructor(
     private httpClient: HttpClient
