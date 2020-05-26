cce058db3a7881f65b985d7f98e4c2df9f2517cd
Sipann <vauguste92@gmail.com>
2020-05-26 17:41:40 +0200

    style: clean up group manage page

--- client/src/app/groups/group-manage/group-manage.page.ts
+++ client/src/app/groups/group-manage/group-manage.page.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 import { Subscription } from 'rxjs';
-
-import { ApiClientService } from 'src/app/services/api-client.service';
-import { Group } from 'src/app/models/group.model';
-import { Item } from '../../models/item.model';
-import { OrderSumup } from 'src/app/models/order-sumup.model';
-import { User } from '../../models/user.model';
-
 import { map } from 'rxjs/operators';
-import { Store, select } from '@ngrx/store';
-import { AppState } from '../../store/reducers/index';
-import * as fromGroupsActions from '../../store/actions/groups.actions';
+
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/reducers';
+
+import { Group } from 'src/app/models/group.model';
 
 
 @Component({
@@ -22,34 +18,16 @@ import * as fromGroupsActions from '../../store/actions/groups.actions';
 })
 export class GroupManagePage implements OnInit, OnDestroy {
 
-  loading_infos = true;
-  loading_items = true;
-  loading_members = true;
-  loading_summary = true;
-
-  groupId: number;
-  managerName: string;
-  managing: '' | 'info' | 'products' | 'summary' | 'users';
-  members: User[];
-  summaryByItem: {}[];
-  summaryByUser: {}[];
-
-  items: Item[];
-  group: Group;
-
-  private infosSub: Subscription;
-  private itemsSub: Subscription;
-  private membersSub: Subscription;
-  private summarySub: Subscription;
-
+  public group: Group;
+  public groupid: string;
   private groupSub: Subscription;
+  public managing: '' | 'info' | 'products' | 'summary' | 'users';
 
   constructor(
-    private route: ActivatedRoute,
-    private apiClientService: ApiClientService,
     private navCtrl: NavController,
-    private router: Router,
-    private store: Store<AppState>) { }
+    private route: ActivatedRoute,
+    private store: Store<AppState>,
+  ) { }
 
 
   ngOnInit() {
@@ -59,90 +37,24 @@ export class GroupManagePage implements OnInit, OnDestroy {
         return;
       }
 
+      this.groupid = paramMap.get('groupid');
+
       this.groupSub = this.store.select('groups')
-        .pipe(map(g => g.selectedGroup))
-        .subscribe(selectedGroupData => {
-          this.group = selectedGroupData;
-        })
-
-
-      if (this.router.getCurrentNavigation().extras.state) {
-        const group = this.router.getCurrentNavigation().extras.state;
-        this.group = Group.parse(group);
-
-        this.loading_infos = false;
-        // this.fetchItems();
-        // this.fetchMembers();
-        // this.fetchSummary();
-
-
-      }
-      else {
-        this.router.navigate(['/', 'groups', 'detail', this.groupId]);
-        return;
-      }
-
+        .pipe(map(groups => groups.groups))
+        .subscribe(groups => {
+          this.group = groups.find(group => group.id === this.groupid);
+        });
     });
   }
 
   ngOnDestroy() {
-    if (this.infosSub) this.infosSub.unsubscribe();
-    if (this.itemsSub) this.itemsSub.unsubscribe();
-    if (this.membersSub) this.membersSub.unsubscribe();
-    if (this.summarySub) this.summarySub.unsubscribe();
+    if (this.groupSub) this.groupSub.unsubscribe();
   }
-
-
-  // fetchGroupInfos() {
-  //   this.infosSub = this.apiClientService.getGroup(this.group.id)
-  //     .subscribe(data => {
-  //       this.group = data;
-  //     });
-  // }
-
-  // fetchItems() {
-  //   this.itemsSub = this.apiClientService.getGroupItems(this.group.id)
-  //     .subscribe(data => {
-  //       this.items = data;
-  //       this.loading_items = false;
-  //     });
-  // }
-
-  // fetchMembers() {
-  //   this.membersSub = this.apiClientService.getGroupMembers(this.group.id)
-  //     .subscribe(data => {
-  //       this.members = data;
-  //       this.loading_members = false;
-  //       const manager = data.find(member => member.id === this.group.manager_id);
-  //       this.managerName = manager.name;
-  //     });
-  // }
-
-  // fetchSummary() {
-  //   if (this.group.deadline) {
-  //     this.summarySub = this.apiClientService.getGroupOrder(this.group.id, this.group.deadline)
-  //       .subscribe(data => {
-  //         this.summaryByUser = this.reduceByUser(data);
-  //         this.summaryByItem = this.reduceByItem(data);
-  //         this.loading_summary = false;
-  //       });
-  //   }
-
-  // }
 
   onCancel() { this.managing = ''; }
-
-  onDone(group: Group) {
-    this.managing = '';
-    this.group = group;
-    // this.fetchSummary();
-  }
-
-  onItemsUpdated() { this.fetchItems(); }
 
   onSelect(managing: '' | 'info' | 'products' | 'summary' | 'users') {
     this.managing = managing;
   }
-
 
 }
