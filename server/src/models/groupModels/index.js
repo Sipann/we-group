1d4fd545709f7e9abef07db7c80aa751f047b3b2
Sipann <vauguste92@gmail.com>
2020-09-27 19:20:24 +0200

    SERVER - groupModels index clean up

--- server/src/models/groupModels/index.js
+++ server/src/models/groupModels/index.js
@@ -1,16 +1,14 @@
-export { addUserToGroup as DBAddUserToGroup } from './addUserToGroup';
-export { createGroup as DBCreateGroup } from './createGroup';
-export { fetchGroupMembers as DBFetchGroupMembers } from './fetchGroupMembers';
-export { searchGroups as DBSearchGroups } from './searchGroups';
-export { updateGroupInfos as DBUpdateGroupInfos } from './updateGroupInfos';
-export { createNewGroupOrder as DBCreateNewGroupOrder } from './createNewGroupOrder';
-export { addNewItemToAvailableOrder as DBAddNewItemToAvailableOrder } from './addNewItemToAvailableOrder';
 export { addExistingItemToAvailableOrder as DBAddExistingItemToAvailableOrder } from './addExistingItemToAvailableOrder';
-export { fetchGroupFullAvailableOrders as DBFetchGroupFullAvailableOrders } from './fetchGroupFullAvailableOrders';
-export { deleteGroupAvailableOrder as DBDeleteGroupAvailableOrder } from './deleteGroupAvailableOrder';
+export { addNewItemToAvailableOrder as DBAddNewItemToAvailableOrder } from './addNewItemToAvailableOrder';
+export { addUserToGroup as DBAddUserToGroup } from './addUserToGroup';
 export { changeGroupManager as DBChangeGroupManager } from './changeGroupManager';
-export { updateAvailableOrderInfos as DBUpdateAvailableOrderInfos } from './updateAvailableOrderInfos';
-
-export { removeMemberFromGroup as DBRemoveMemberFromGroup } from './removeMemberFromGroup';
+export { createGroup as DBCreateGroup } from './createGroup';
+export { createNewGroupOrder as DBCreateNewGroupOrder } from './createNewGroupOrder';
 export { deleteGroup as DBDeleteGroup } from './deleteGroup';
-export { updateGroupAvailableOrder as DBUpdateGroupAvailableOrder } from './updateGroupAvailableOrder';
+export { deleteGroupAvailableOrder as DBDeleteGroupAvailableOrder } from './deleteGroupAvailableOrder';
+export { fetchGroupFullAvailableOrders as DBFetchGroupFullAvailableOrders } from './fetchGroupFullAvailableOrders';
+export { fetchGroupMembers as DBFetchGroupMembers } from './fetchGroupMembers';
+export { removeMemberFromGroup as DBRemoveMemberFromGroup } from './removeMemberFromGroup';
+export { searchGroups as DBSearchGroups } from './searchGroups';
+export { updateAvailableOrderInfos as DBUpdateAvailableOrderInfos } from './updateAvailableOrderInfos';
+export { updateGroupInfos as DBUpdateGroupInfos } from './updateGroupInfos';
