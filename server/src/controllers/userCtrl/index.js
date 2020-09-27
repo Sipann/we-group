7e64cee0860d8908d6147f1db1f81351ce5613d7
Sipann <vauguste92@gmail.com>
2020-09-27 17:32:35 +0200

    SERVER - clean up index exporting files

--- server/src/controllers/userCtrl/index.js
+++ server/src/controllers/userCtrl/index.js
@@ -4,3 +4,4 @@
 export { createUser as CtrlCreateUser } from './createUser';
 export { updateUser as CtrlUpdateUser } from './updateUser';
 export { deleteUser as CtrlDeleteUser } from './deleteUser';
+export { fetchUserDataCustom as CtrlFetchUserDataCustom } from './fetchUserDataCustom';
--- server/src/models/userModels/index.js
+++ server/src/models/userModels/index.js
@@ -1,9 +1,9 @@
 export { fetchFullGroupsUserIsMemberOf } from './fetchFullGroupsUserIsMemberOf';
 export { fetchLightGroupsUserIsMemberOf } from './fetchLightGroupsUserIsMemberOf';
-export { fetchUserData } from './fetchUserData';
 export { createUser as DBCreateUser } from './createUser';
 export { updateUser as DBUpdateUser } from './updateUser';
 export { removeUserFromGroup as DBRemoveUserFromGroup } from './removeUserFromGroup';
 
 export { fetchGroupFullAvailableOrders as DBFetchGroupFullAvailableOrders } from './fetchGroupFullAvailableOrders';
 export { deleteUser as DBDeleteUser } from './deleteUser';
+export { fetchUserDataCustom as DBFetchUserDataCustom } from './fetchUserDataCustom';
