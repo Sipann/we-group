22ac85e306ddb5fdb7c47b9b60f9fa4a60136796
Sipann <vauguste92@gmail.com>
2020-09-27 18:01:37 +0200

    SERVER - remove user/ removeUserFromGroup flow

--- server/src/models/userModels/index.js
+++ server/src/models/userModels/index.js
@@ -1,9 +1,7 @@
-export { fetchFullGroupsUserIsMemberOf } from './fetchFullGroupsUserIsMemberOf';
-export { fetchLightGroupsUserIsMemberOf } from './fetchLightGroupsUserIsMemberOf';
 export { createUser as DBCreateUser } from './createUser';
-export { updateUser as DBUpdateUser } from './updateUser';
-export { removeUserFromGroup as DBRemoveUserFromGroup } from './removeUserFromGroup';
-
-export { fetchGroupFullAvailableOrders as DBFetchGroupFullAvailableOrders } from './fetchGroupFullAvailableOrders';
 export { deleteUser as DBDeleteUser } from './deleteUser';
+export { fetchFullGroupsUserIsMemberOf } from './fetchFullGroupsUserIsMemberOf';
+export { fetchGroupFullAvailableOrders as DBFetchGroupFullAvailableOrders } from './fetchGroupFullAvailableOrders';
+export { fetchLightGroupsUserIsMemberOf } from './fetchLightGroupsUserIsMemberOf';
 export { fetchUserDataCustom as DBFetchUserDataCustom } from './fetchUserDataCustom';
+export { updateUser as DBUpdateUser } from './updateUser';
